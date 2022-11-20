c82bc329ef7cc09c345e68aee24ef2a1ff7ecd1a
khj5014 <gmlwo5011@naver.com>
2022-11-20 18:02:50 +0900

    221120 타입 지정
    선택적 타입
    Alias 타입 생성
    argument 타입 지정

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
-const gmlwo = {
-    name: "gmlwo"
-}
-console.log(gmlwo.name)
-let a = ""
-console.log(a = "Change")
+let x = ""
+console.log(x = "Change")
 
 let asdf: string[] = ['kim', 'gml', 'wo']
 console.log(asdf)
+
+let a: number[]= [1,2];
+let b: string[] = ["il","1"]
+let c: boolean[] = [true]
+console.log(a,b,c)
+
+const playerX: {
+    name:string,
+    age?:number; //선택적 타입 다루기 "?"
+}={
+    name:"gmlwo"
+}
+console.log(playerX)
+
+if(playerX.age && playerX.age < 10){
+
+}
+
+//Alias 타입 생성법
+type player = {
+    name:string,
+    age?:number
+}
+
+const gmlwo:player = {
+    name:"gmlwo"
+}
+console.log(gmlwo)
+
+//argument 타입 지정방법
+function playerMaker(name:string) : player{
+    return{
+        name,
+    }
+}
+const gmlwo2 = playerMaker("gmlwo123")
+gmlwo2.age = 12
+
+const playerMaker2 = (name:string):player =>({name})
+const gmlwo3 = playerMaker2("gmlwo123")
+gmlwo3.age = 12
