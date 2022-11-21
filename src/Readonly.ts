699cf5874636bad46324141742b77b335d63389a
khj5014 <gmlwo5011@naver.com>
2022-11-21 13:28:33 +0900

    read only
    Read only 배움

---
+++ src/Readonly.ts
@@ -0,0 +1,11 @@
+import { playerMaker2 } from "."
+
+const P2 = playerMaker2("gmlwo123", 25)
+// console.log(P2)
+
+const numbers: readonly number[] = [1, 2, 3, 4]
+// numbers.push(1) //불가
+// console.log(numbers);
+
+const player: readonly [string, number, boolean] = ["gm", 1, true]
+console.log(player);
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,44 @@
 let x = ""
-console.log(x = "Change")
+// console.log(x = "Change")
 
 let asdf: string[] = ['kim', 'gml', 'wo']
-console.log(asdf)
+// console.log(asdf)
 
-let a: number[]= [1,2];
-let b: string[] = ["il","1"]
+let a: number[] = [1, 2];
+let b: string[] = ["il", "1"]
 let c: boolean[] = [true]
-console.log(a,b,c)
+// console.log(a, b, c)
 
 const playerX: {
-    name:string,
-    age?:number; //선택적 타입 다루기 "?"
-}={
-    name:"gmlwo"
+    name: string,
+    age?: number; //선택적 타입 다루기 "?"
+} = {
+    name: "gmlwo"
 }
-console.log(playerX)
+// console.log(playerX)
 
-if(playerX.age && playerX.age < 10){
+if (playerX.age && playerX.age < 10) {
 
 }
 
 //Alias 타입 생성법
-type player = {
-    name:string,
-    age?:number
+type Player = {
+    name: string,
+    age?: number
 }
 
-const gmlwo:player = {
-    name:"gmlwo"
+const gmlwo: Player = {
+    name: "gmlwo"
 }
-console.log(gmlwo)
+// console.log(gmlwo)
 
 //argument 타입 지정방법
-function playerMaker(name:string) : player{
-    return{
+export function playerMaker(name: string): Player {
+    return {
         name,
     }
 }
-const gmlwo2 = playerMaker("gmlwo123")
-gmlwo2.age = 12
 
-const playerMaker2 = (name:string):player =>({name})
-const gmlwo3 = playerMaker2("gmlwo123")
-gmlwo3.age = 12
+export const playerMaker2 = (name: string , age:number): Player => ({ name , age})
+const P1 = playerMaker2("HJ",25)
+// console.log(P1)
