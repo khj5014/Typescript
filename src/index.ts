b9716fbe4b36c91bff1c6f894a64847795328172
khj5014 <gmlwo5011@naver.com>
2022-11-27 04:04:30 +0900

    모듈 재설치
    모듈 재설치

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 let x = ""
-// console.log(x = "Change")
+console.log(x = "Change")
 
 let asdf: string[] = ['kim', 'gml', 'wo']
 // console.log(asdf)
@@ -7,7 +7,7 @@ let asdf: string[] = ['kim', 'gml', 'wo']
 let a: number[] = [1, 2];
 let b: string[] = ["il", "1"]
 let c: boolean[] = [true]
-// console.log(a, b, c)
+console.log(a, b, c)
 
 const playerX: {
     name: string,
